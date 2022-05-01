d354227b2ede28db33750e957cef3a1753c6205a
dywfwd30 <ywang421@my.bcit.ca>
2022-05-01 15:30:09 -0700

    added NextPage buttons and finished heart button

--- src/components/FavButton.js
+++ src/components/FavButton.js
@@ -1,20 +1,24 @@
 // Fav Button
+import IconButton from '@mui/material/IconButton';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
 
 function FavButton({ movieObj, remove, handleFavClick }) {
 
     function handleAddFav(){
         handleFavClick(true, movieObj);
+        console.log("addFav");
     }
 
     function handleRemoveFav(){
         handleFavClick(false, movieObj);
+        console.log("removeFav");
     }
 
     return (
         <>
-            {remove === false ? 
-            <button onClick={handleAddFav}>Add To Favs</button> : 
-            <button onClick={handleRemoveFav}>Remove From Favs</button>}
+            {remove === false ? (<IconButton onClick={handleAddFav} > <StarBorderIcon sx={{ fontSize: "3.5rem", color:"red" }}/> </IconButton>):(<IconButton onClick={handleRemoveFav}><StarIcon sx={{ fontSize: "3.5rem", color:"red" }}/></IconButton>)}
+            
         </>
     );
     
@@ -25,3 +29,6 @@ FavButton.defaultProps = {
 }
 
 export default FavButton;
+            // <button onClick={handleAddFav}>Add To Favs</button> : 
+            // <button onClick={handleRemoveFav}>Remove From Favs</button>
+
--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,40 +1,72 @@
 import { appTitle } from "../globals/globals";
 import { useEffect, useState } from 'react';
-import NavSort from '../components/NavSort';
 import Movies from '../components/Movies';
 import { API_TOKEN } from '../globals/globals';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 
 function PageHome({ sort }) {
     useEffect(() => {
       document.title = `${appTitle} - Home`;
     }, []);
 
+    const [moviesPage, setMoviesPage] = useState(1);
     const [moviesData, setMoviesData] = useState(null);
 
     useEffect(() => {
 
         const fetchMovies = async () => {
-            const res = await fetch(`https://api.themoviedb.org/3/movie/${sort}?language=en-US&page=1`, {
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${sort}?language=en-US&page=${moviesPage}`, {
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
                     'Authorization': 'Bearer ' + API_TOKEN
                   }
             });
+            console.log("below are for fetchMovies");
+            console.log(fetchMovies);
             const moviesData = await res.json();
-            const first12Movies = moviesData.results.splice(0,12); 
+            const first12Movies = moviesData.results; 
             console.log(first12Movies);
             setMoviesData(first12Movies);
           }
       
           fetchMovies();
 
-    }, [sort]);
+    }, [sort, moviesPage]);
+
+    function nextPage(){
+        var moviesPageCopy = moviesPage;
+        moviesPageCopy++;
+        setMoviesPage(moviesPageCopy);
+    }
+    function prevPage(){
+        var moviesPageCopy = moviesPage;
+        if(moviesPageCopy !== 1)
+            moviesPageCopy--;
+        setMoviesPage(moviesPageCopy);
+    }
 
     return (
         <section className="home-page">
-            <NavSort />
             {moviesData !== null && <Movies moviesData={moviesData} />}
+            {moviesPage == 1 ?
+                (<Stack direction="row" spacing={2}>
+                    <Button onClick={nextPage} variant="contained" endIcon={<NavigateNextIcon />}>
+                        Next Page
+                    </Button>
+                </Stack>) :
+                (<Stack direction="row" spacing={2}>
+                    <Button onClick={prevPage} variant="contained" startIcon={<NavigateBeforeIcon/>}>
+                        Prev Page
+                    </Button>
+                    <Button onClick={nextPage} variant="contained" endIcon={<NavigateNextIcon />}>
+                        Next Page
+                    </Button>
+                </Stack>)
+            }
         </section>
     )
 }
